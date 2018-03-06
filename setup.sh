7c5ac66116ec50431dbd950d5669827ac612cfad
Rui Miguel Silva Seabra <rms@roque.1407.org>
2018-03-06 19:54:21 +0000

    Expand setup script to prepare the environment and the volumes

--- setup.sh
+++ setup.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
-read -p "Please type your project root: " ROOT
+# Ask setup questions
+read -e -p "Please type your project root: " -i "$(pwd)/blog" ROOT
 
+# Dump the .env file for docker-compose
 cat <<EOF > blog/.env
 ROOT=$ROOT
 EOF
+
+# Prepare the db and wordpress volumes
+mkdir -p blog/files/db
+
+cd blog/files
+
+# Download (if needed) and unpack wordpress
+if [ ! -e latest.tar.gz ]; then
+	wget https://wordpress.org/latest.tar.gz
+fi
+tar xzvf latest.tar.gz
